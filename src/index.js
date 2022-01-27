d535f68432154054a0cccc903e2399d4997a1bb2
KappaStromblessed <seb02tian20@gmail.com>
2022-01-26 18:15:00 -0700

    Limpiando archivos

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
-// import './index.html';
-import { saludar } from './js/componentes';
-import './styles.css';
-import './index.html';
-
-const nombre = 'Dios';
-saludar(nombre);
+import './styles.css'
+import './index.html'
